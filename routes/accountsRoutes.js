a840d7aeb29ab71d98c6d07dd5ef415ac5a323ad
בני סולומון <mr.benny.solomon@gmail.com>
2023-03-24 00:01:21 +0200

    projet ver 1.1

--- routes/accountsRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import {getAccounts,getAccount,addAccount,updateAccount,deleteAccount} from '../controllers/accountsController.js';
-
-const router = express.Router({ mergeParams: true });
-
-router.route('/').get(getAccounts).post(addAccount);
-router.route('/:id').get(getAccount).put(updateAccount).delete(deleteAccount);
-
-export default router;
